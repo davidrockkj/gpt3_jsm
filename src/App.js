e7b6e85ce2193a6dbffdca44a3eefd945a46a34d
davidrockkj <davidrockkj@gmail.com>
2023-11-08 12:11:54 -0300

    Criação de pastas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>GPT-3</h1>
     </div>
   );
 }

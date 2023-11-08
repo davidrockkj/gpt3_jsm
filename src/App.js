bf95af3693e103cf8e2b8463dde7a93b71f2a35e
davidrockkj <davidrockkj@gmail.com>
2023-11-08 13:41:53 -0300

    Criado o CSS do App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './Containers'
 import { Cta, Brand, Navbar } from './Components'

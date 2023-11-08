b8a5b069b12905ccb74686b4f3813c91640eacd3
davidrockkj <davidrockkj@gmail.com>
2023-11-08 14:03:48 -0300

    Personalizando textos da navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../Assets/logo.svg'
 import './navbar.css'
 
@@ -10,11 +10,11 @@ const Navbar = () => {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alto='logo' />
+          <img src={logo} alt='logo' />
         </div>
-        <div className='.gpt3__navbar-links-container'>
+        <div className='gpt3__navbar-links_container'>
           <p><a href='#home'>HOME</a></p>
-          <p><a href='#wgpt3'>What is GPT3</a></p>
+          <p><a href='#wgpt3'>What is GPT3?</a></p>
           <p><a href='#possibility'>Open AI</a></p>
           <p><a href='#features'>Case Studies</a></p>
           <p><a href='#blog'>Library</a></p>

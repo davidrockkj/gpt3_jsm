92195c0e7dde051c0a843dd15096943c3ce0223f
davidrockkj <davidrockkj@gmail.com>
2023-11-10 12:23:38 -0300

    Adicionando texto de descrição, input email, button get started

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -9,6 +9,14 @@ const Header = () => {
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Adress' />
+          <button type='button'>Get Started</button>
+        </div>
       </div>
     </div>
   )

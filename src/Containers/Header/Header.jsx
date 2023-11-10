f83f2bb2346c5f74c308865c36974a29b63ea83f
davidrockkj <davidrockkj@gmail.com>
2023-11-10 12:20:03 -0300

    Adicionando H1 do header com grandient

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './header.css'
+import '../../App.css'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
     </div>
   )
 }
